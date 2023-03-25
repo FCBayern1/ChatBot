5acafbb573437fe7cbd8f5551a59595efe653db7
joshua <292387082@qq.com>
2023-03-25 22:59:39 +0000

    joshua,feat:initalize project

---
+++ ChatBot-interfaces/src/main/java/com/josh/ai/ChatBot/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.josh.ai.ChatBot.api;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//@description entrance
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ ChatBot-interfaces/src/test/java/com/josh/ai/ChatBot/api/test/ApiTest.java
@@ -0,0 +1,9 @@
+package com.josh.ai.ChatBot.api.test;
+
+/**
+ * Description: Test
+ * Author: joshua
+ * Date: 2023/3/25
+ */
+public class ApiTest {
+}
